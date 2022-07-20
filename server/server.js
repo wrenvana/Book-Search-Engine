d5c96fcf5ea958b81bc8c33128b0059d448b9ec7
Wren Sanchez <demisa00@gmail.com>
2022-07-19 19:44:28 -0700

    Trying to put server.js in order

--- server/server.js
+++ server/server.js
@@ -14,15 +14,21 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-server.applyMiddleware({ app })
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// const startApolloServer = async (typeDefs, resolvers) => {
-//   await server.start();
-//   server.applyMiddleware({ app });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
   db.once("open", () => {
     app.listen(PORT, () => {
@@ -32,13 +38,5 @@ server.applyMiddleware({ app })
       );
     });
   });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
 startApolloServer(typeDefs, resolvers);
