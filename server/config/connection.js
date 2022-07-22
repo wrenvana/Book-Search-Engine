54c3b58cf0065d987037cca8bebefd4ff8231de0
Wren Sanchez <demisa00@gmail.com>
2022-07-21 17:22:30 -0700

    Connection debugging

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
-const { connect } = require('mongoose');
 const mongoose = require('mongoose');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
