e6911978ebbacc924ad15767779f6f8059dfd464
Wren Sanchez <demisa00@gmail.com>
2022-07-21 18:21:39 -0700

    .env

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require('./utils/auth');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
