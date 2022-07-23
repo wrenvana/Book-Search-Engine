5b5b5a6dc450172d6d3a1754794821e09b4b8fb2
Wren Sanchez <demisa00@gmail.com>
2022-07-22 20:19:42 -0700

    edits

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,12 +4,6 @@ require('dotenv').config({ path: '../.env' });
 
 mongoose.connect(
   process.env.MONGO_DB_URI || 'mongodb://127.0.0.1:27017/googlebooks',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
 );
 
 module.exports = mongoose.connection;
