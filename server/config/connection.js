30e1b4c337aebb601d47b6499555fdc1bbff9d75
Wren Sanchez <102892018+wrenvana@users.noreply.github.com>
2022-07-22 16:21:07 -0800

    Update connection.js
    Updating from laptop.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,20 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
-
-const connection = mongoose.connection;
-connection.on("connected", function () {
-  console.log("Connection successful.");
-});
-connection.on("disconnected", function () {
-  console.log("Disconnected successfully.");
-});
-connection.on("error", console.error.bind(console, "Connection error:"));
 
 module.exports = connection;
