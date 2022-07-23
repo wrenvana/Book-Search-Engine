e1e7f04dcdd0953e88bb0f9c787933288580b935
Wren Sanchez <102892018+wrenvana@users.noreply.github.com>
2022-07-22 16:20:18 -0800

    Update auth.js
    Updating from laptop.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,3 @@
-// TODO: Update auth middleware function to work with GraphQL API
-
-
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -9,26 +6,23 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return req;
     }
 
     // send to next endpoint
